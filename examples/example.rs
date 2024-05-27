1da44022b4f5f59ff754102cb24663d78182ba99
Justin de Ruiter <justinderuiter@ziggo.nl>
2024-05-27 12:38:12 +0200

    Textures

--- examples/example.rs
+++ examples/example.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs::File,
     iter,
     mem::size_of,
     num::NonZeroU64,
@@ -7,23 +8,31 @@ use std::{
 };
 
 use bytemuck::{cast_slice, NoUninit};
+use ddsfile::{Dds, DxgiFormat};
 use futures::executor::block_on;
 use gamebox::{
-    engines::{game_data::item::ItemModel, plug::visual_indexed_triangles::Indices},
+    engines::{
+        game_data::item::ItemModel,
+        plug::{material::Material, visual_indexed_triangles::Indices},
+    },
+    read::HeaderOptions,
     Item,
 };
 use glam::{Mat4, Vec3};
 use wgpu::{
     include_wgsl,
-    util::{BufferInitDescriptor, DeviceExt},
+    util::{BufferInitDescriptor, DeviceExt, TextureDataOrder},
     vertex_attr_array, Adapter, BindGroup, BindGroupDescriptor, BindGroupEntry,
     BindGroupLayoutDescriptor, BindGroupLayoutEntry, BindingResource, BindingType, Buffer,
     BufferBinding, BufferBindingType, BufferDescriptor, BufferUsages, ColorTargetState,
-    ColorWrites, CommandEncoderDescriptor, Device, DeviceDescriptor, FragmentState, IndexFormat,
-    Instance, InstanceDescriptor, MultisampleState, Operations, PipelineCompilationOptions,
-    PipelineLayoutDescriptor, PrimitiveState, Queue, RenderPassColorAttachment,
-    RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor, RequestAdapterOptions,
-    ShaderStages, Surface, TextureViewDescriptor, VertexBufferLayout, VertexState, VertexStepMode,
+    ColorWrites, CommandEncoderDescriptor, Device, DeviceDescriptor, Extent3d, Features,
+    FragmentState, IndexFormat, Instance, InstanceDescriptor, Limits, MultisampleState, Operations,
+    PipelineCompilationOptions, PipelineLayoutDescriptor, PrimitiveState, Queue,
+    RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor,
+    RequestAdapterOptions, Sampler, SamplerBindingType, SamplerDescriptor, ShaderStages, Surface,
+    Texture, TextureDescriptor, TextureDimension, TextureFormat, TextureSampleType, TextureUsages,
+    TextureView, TextureViewDescriptor, TextureViewDimension, VertexBufferLayout, VertexState,
+    VertexStepMode,
 };
 use winit::{
     application::ApplicationHandler,
@@ -36,11 +45,7 @@ use winit::{
 fn main() {
     env_logger::init();
 
-    let mut app = App {
-        window: None,
-        renderer: None,
-        last_render_time: Instant::now(),
-    };
+    let mut app = App { state: None };
 
     let event_loop = EventLoop::new().unwrap();
     event_loop.set_control_flow(ControlFlow::Poll);
@@ -48,9 +53,7 @@ fn main() {
 }
 
 struct App {
-    window: Option<Arc<Window>>,
-    renderer: Option<Renderer>,
-    last_render_time: Instant,
+    state: Option<State>,
 }
 
 impl ApplicationHandler for App {
@@ -60,8 +63,6 @@ impl ApplicationHandler for App {
                 .create_window(Window::default_attributes())
                 .unwrap(),
         );
-
-        self.window = Some(Arc::clone(&window));
 
         let instance = Instance::new(InstanceDescriptor::default());
 
@@ -73,8 +74,15 @@ impl ApplicationHandler for App {
         }))
         .unwrap();
 
-        let (device, queue) =
-            block_on(adapter.request_device(&DeviceDescriptor::default(), None)).unwrap();
+        let (device, queue) = block_on(adapter.request_device(
+            &DeviceDescriptor {
+                label: None,
+                required_features: Features::TEXTURE_COMPRESSION_BC,
+                required_limits: Limits::default(),
+            },
+            None,
+        ))
+        .unwrap();
 
         let window_size = window.inner_size();
 
@@ -99,9 +107,32 @@ impl ApplicationHandler for App {
                 }],
             });
 
+        let texture_bind_group_layout =
+            device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+                label: None,
+                entries: &[
+                    BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: ShaderStages::FRAGMENT,
+                        ty: BindingType::Texture {
+                            sample_type: TextureSampleType::Float { filterable: true },
+                            view_dimension: TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    },
+                    BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: ShaderStages::FRAGMENT,
+                        ty: BindingType::Sampler(SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+            });
+
         let render_pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
             label: None,
-            bind_group_layouts: &[&camera_bind_group_layout],
+            bind_group_layouts: &[&camera_bind_group_layout, &texture_bind_group_layout],
             push_constant_ranges: &[],
         });
 
@@ -117,7 +148,7 @@ impl ApplicationHandler for App {
                 buffers: &[VertexBufferLayout {
                     array_stride: size_of::<Vertex>() as u64,
                     step_mode: VertexStepMode::Vertex,
-                    attributes: &vertex_attr_array![0 => Float32x3],
+                    attributes: &vertex_attr_array![0 => Float32x3, 1 => Float32x2],
                 }],
             },
             primitive: PrimitiveState::default(),
@@ -158,7 +189,7 @@ impl ApplicationHandler for App {
 
         let camera = (Camera::new(), camera_buffer, camera_bind_group);
 
-        let item: Item = gamebox::read_file("examples/big_palm_tree_low.Item.Gbx").unwrap();
+        let item: Item = gamebox::read_file("big_palm_tree_low.Item.Gbx").unwrap();
 
         let mut buffers = vec![];
 
@@ -168,12 +199,17 @@ impl ApplicationHandler for App {
 
                 for mesh in model.meshes() {
                     let positions = mesh.vertex_stream().positions();
+                    let texcoords = mesh.vertex_stream().texcoords();
 
                     let mut vertices = vec![];
 
-                    for position in positions {
+                    for i in 0..positions.len() {
+                        let position = &positions[i];
+                        let texcoord = &texcoords[i];
+
                         vertices.push(Vertex {
                             pos: [position.x, position.y, position.z],
+                            texcoord: [texcoord.x, texcoord.y],
                         });
                     }
 
@@ -201,14 +237,92 @@ impl ApplicationHandler for App {
             _ => panic!(),
         }
 
-        self.renderer = Some(Renderer {
-            surface,
-            adapter,
-            device,
-            queue,
-            render_pipeline,
-            camera,
-            buffers,
+        let material: Material = gamebox::Reader::new()
+            .read_header(HeaderOptions::Skip {
+                assume_size_zero: true,
+            })
+            .read_file("GameData/Stadium/Media/Modifier/PlatformGrass/PlatformTech.Material.Gbx")
+            .unwrap();
+
+        let texture_path = material.custom().unwrap().diffuse_texture_path();
+
+        let texture: gamebox::engines::plug::texture::Texture = gamebox::Reader::new()
+            .read_header(HeaderOptions::Skip {
+                assume_size_zero: true,
+            })
+            .read_file(format!("GameData/{}", texture_path.display()))
+            .unwrap();
+
+        let image_path = texture.image_path();
+
+        let image = Dds::read(
+            File::open(format!(
+                "GameData/Stadium/Media/Texture/{}",
+                image_path.display()
+            ))
+            .unwrap(),
+        )
+        .unwrap();
+
+        let texture_format = match image.get_dxgi_format().unwrap() {
+            DxgiFormat::BC1_UNorm_sRGB => TextureFormat::Bc1RgbaUnormSrgb,
+            _ => panic!(),
+        };
+
+        let texture = device.create_texture_with_data(
+            &queue,
+            &TextureDescriptor {
+                label: None,
+                size: Extent3d {
+                    width: image.get_width(),
+                    height: image.get_height(),
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: TextureDimension::D2,
+                format: texture_format,
+                usage: TextureUsages::TEXTURE_BINDING,
+                view_formats: &[],
+            },
+            TextureDataOrder::default(),
+            &image.get_data(0).unwrap(),
+        );
+
+        let texture_view = texture.create_view(&TextureViewDescriptor::default());
+
+        let sampler = device.create_sampler(&SamplerDescriptor::default());
+
+        let texture_bind_group = device.create_bind_group(&BindGroupDescriptor {
+            label: None,
+            layout: &texture_bind_group_layout,
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: BindingResource::TextureView(&texture_view),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: BindingResource::Sampler(&sampler),
+                },
+            ],
+        });
+
+        let texture = (texture, texture_view, sampler, texture_bind_group);
+
+        self.state = Some(State {
+            window,
+            renderer: Renderer {
+                surface,
+                adapter,
+                device,
+                queue,
+                render_pipeline,
+                camera,
+                buffers,
+                texture,
+            },
+            last_render_time: Instant::now(),
         });
     }
 
@@ -218,27 +332,27 @@ impl ApplicationHandler for App {
         _window_id: WindowId,
         event: WindowEvent,
     ) {
-        let renderer = self.renderer.as_mut().expect("renderer is None");
+        let state = self.state.as_mut().expect("state is None");
 
         match event {
             WindowEvent::Resized(size) => {
-                let renderer = self.renderer.as_ref().expect("renderer is None");
-
-                let surface_config = renderer
+                let surface_config = state
+                    .renderer
                     .surface
-                    .get_default_config(&renderer.adapter, size.width, size.height)
+                    .get_default_config(&state.renderer.adapter, size.width, size.height)
                     .unwrap();
 
-                renderer
+                state
+                    .renderer
                     .surface
-                    .configure(&renderer.device, &surface_config);
+                    .configure(&state.renderer.device, &surface_config);
             }
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
             WindowEvent::KeyboardInput { event, .. } => {
                 if let PhysicalKey::Code(code) = event.physical_key {
-                    renderer.camera.0.process_keyboard(code, event.state);
+                    state.renderer.camera.0.process_keyboard(code, event.state);
                 }
             }
 
@@ -252,30 +366,30 @@ impl ApplicationHandler for App {
         _device_id: DeviceId,
         event: DeviceEvent,
     ) {
-        let renderer = self.renderer.as_mut().expect("renderer is None");
+        let state = self.state.as_mut().expect("state is None");
 
         if let DeviceEvent::MouseMotion { delta } = event {
-            renderer.camera.0.process_mouse(delta.0, delta.1);
+            state.renderer.camera.0.process_mouse(delta.0, delta.1);
         }
     }
 
     fn about_to_wait(&mut self, _event_loop: &ActiveEventLoop) {
+        let state = self.state.as_mut().expect("state is None");
+
         let now = Instant::now();
-        let dt = now.duration_since(self.last_render_time);
-        self.last_render_time = now;
+        let dt = now.duration_since(state.last_render_time);
+        state.last_render_time = now;
 
-        let window = self.window.as_ref().expect("window is None");
-        let renderer = self.renderer.as_mut().expect("renderer is None");
+        state.renderer.camera.0.update_camera(dt);
 
-        renderer.camera.0.update_camera(dt);
+        let window_size = state.window.inner_size();
 
-        let window_size = window.inner_size();
-
-        renderer.queue.write_buffer(
-            &renderer.camera.1,
+        state.renderer.queue.write_buffer(
+            &state.renderer.camera.1,
             0,
             cast_slice(
-                &renderer
+                &state
+                    .renderer
                     .camera
                     .0
                     .matrix(window_size.width as f32 / window_size.height as f32)
@@ -283,13 +397,14 @@ impl ApplicationHandler for App {
             ),
         );
 
-        let surface_texture = renderer.surface.get_current_texture().unwrap();
+        let surface_texture = state.renderer.surface.get_current_texture().unwrap();
 
         let surface_texture_view = surface_texture
             .texture
             .create_view(&TextureViewDescriptor::default());
 
-        let mut command_encoder = renderer
+        let mut command_encoder = state
+            .renderer
             .device
             .create_command_encoder(&CommandEncoderDescriptor::default());
 
@@ -306,11 +421,12 @@ impl ApplicationHandler for App {
                 occlusion_query_set: None,
             });
 
-            render_pass.set_pipeline(&renderer.render_pipeline);
+            render_pass.set_pipeline(&state.renderer.render_pipeline);
 
-            render_pass.set_bind_group(0, &renderer.camera.2, &[]);
+            render_pass.set_bind_group(0, &state.renderer.camera.2, &[]);
+            render_pass.set_bind_group(1, &state.renderer.texture.3, &[]);
 
-            for (vertex_buffer, index_buffer, num_indices) in &renderer.buffers {
+            for (vertex_buffer, index_buffer, num_indices) in &state.renderer.buffers {
                 render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
                 render_pass.set_index_buffer(index_buffer.slice(..), IndexFormat::Uint16);
                 render_pass.draw_indexed(0..*num_indices, 0, 0..1);
@@ -319,10 +435,16 @@ impl ApplicationHandler for App {
 
         let command_buffer = command_encoder.finish();
 
-        renderer.queue.submit(iter::once(command_buffer));
+        state.renderer.queue.submit(iter::once(command_buffer));
 
         surface_texture.present();
     }
+}
+
+struct State {
+    window: Arc<Window>,
+    renderer: Renderer,
+    last_render_time: Instant,
 }
 
 struct Renderer {
@@ -333,12 +455,14 @@ struct Renderer {
     render_pipeline: RenderPipeline,
     camera: (Camera, Buffer, BindGroup),
     buffers: Vec<(Buffer, Buffer, u32)>,
+    texture: (Texture, TextureView, Sampler, BindGroup),
 }
 
 #[repr(C)]
 #[derive(Clone, Copy, NoUninit)]
 struct Vertex {
     pos: [f32; 3],
+    texcoord: [f32; 2],
 }
 
 struct Camera {
--- examples/example.wgsl
+++ examples/example.wgsl
@@ -7,21 +7,30 @@ var<uniform> camera: Camera;
 
 struct VertexInput {
     @location(0) position: vec3<f32>,
+    @location(1) texcoord: vec2<f32>,
 };
 
 struct VertexOutput {
     @builtin(position) position: vec4<f32>,
+    @location(0) texcoord: vec2<f32>,
 };
 
 @vertex
 fn vert_main(input: VertexInput) -> VertexOutput {
     var output: VertexOutput;
     output.position = camera.view_proj * vec4<f32>(input.position, 1.0);
+    output.texcoord = input.texcoord;
 
     return output;
 }
 
+@group(1) @binding(0)
+var diffuse_texture: texture_2d<f32>;
+
+@group(1) @binding(1)
+var diffuse_sampler: sampler;
+
 @fragment
-fn frag_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+fn frag_main(input: VertexOutput) -> @location(0) vec4<f32> {
+    return textureSample(diffuse_texture, diffuse_sampler, input.texcoord);
 }
